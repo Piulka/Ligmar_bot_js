613b91258ac026bdff8732b6666add5bd116d17b
Павел Сироткин <piul@MacBook-Pro-Pavel.local>
2025-06-02 16:41:48 +0300

    Обновлена версия до v.3.6.2

--- modules/config.js
+++ modules/config.js
@@ -30,7 +30,7 @@ window.BotConfig = {
     dropPotionEnabled: false,
 
     // Версия скрипта
-    SCRIPT_COMMIT: 'v.3.6.1',
+    SCRIPT_COMMIT: 'v.3.6.2',
 
     // Навыки для каждого класса
     CLASS_SKILLS: {
